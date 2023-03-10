1a5458378865588499ffde4e6c54f2775c52d97f
Amani Kashema <amanikashema000@gmail.com>
2023-03-10 15:43:42 +0200

    added logger object

--- src/test/java/com/inetbanking/testCases/BaseClass.java
+++ src/test/java/com/inetbanking/testCases/BaseClass.java
@@ -1,20 +1,28 @@
 package com.inetbanking.testCases;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.util.logging.Logger;
 
 public class BaseClass {
     public String basUrl = "https://demo.guru99.com/v3/index.php";
     public String username = "mngr484312";
     public String password = "AdunUhA";
     public static WebDriver driver;
+    public static  Logger logger;
 
     @BeforeClass
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        logger = Logger.getLogger("ebanking");
+        PropertyConfigurator.configure("Log4j.properties");
+
     }
 
     @AfterClass
--- src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
+++ src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
@@ -8,18 +8,23 @@ public class TC_LoginTest_001 extends BaseClass {
     @Test
     public void loginTest(){
         driver.get(basUrl);
+        logger.info("URL is Opened");
 
         LoginPage lp = new LoginPage(driver);
         lp.setUserName(username);
+        logger.info("Entered Username");
         lp.setPassword(password);
+        logger.info("Entered Password");
         lp.clickSubmit();
 
         if(driver.getTitle().equals("Guru99 Bank Manager HomePage")){
             Assert.assertTrue(true);
+            logger.info("Login Test Passed");
             System.out.println(driver.getTitle());
         }
         else{
             Assert.fail();
+            logger.info("Login Test Failed");
         }
 
     }
